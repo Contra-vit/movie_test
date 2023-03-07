1a286df137ba4c4ffaf3e612a47a802b6cb082b8
vladimirtsaran <vladimir.tsaran.x@gmail.com>
2023-03-07 14:07:55 +0500

    TextAria and List MUI

--- src/components/asycBox/Asynchronous.tsx
+++ src/components/asycBox/Asynchronous.tsx
@@ -10,7 +10,17 @@ import { Link } from '@mui/material';
 import About from "../about/About";
 import {useState, useContext} from "react";
 import MovieContext from '../MovieContext'
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import ImageIcon from '@mui/icons-material/Image';
+import WorkIcon from '@mui/icons-material/Work';
+import BeachAccessIcon from '@mui/icons-material/BeachAccess';
+import Divider from '@mui/material/Divider';
 
+import  './Asynchronous.css'
 interface Film {
     Title: string;
     Year: number;
@@ -27,12 +37,15 @@ export default function Asynchronous() {
     const loading = open && options.length === 0;
     const [movieLink, setMovieLink] =  useState('');
     const {inputAutoValue, setInputAutoValue} = useContext(MovieContext)
-    console.log(inputValue)
+
+    console.log(inputAutoValue)
+
     React.useEffect(() => {
         let active = true;
         if (!inputValue) {
             return undefined;
         }
+
         (async () => {
             let topFilms = [];
             try {
@@ -57,6 +70,7 @@ export default function Asynchronous() {
             // console.log('value = ', value)
             // console.log('inputValue = ', inputValue)
             // console.log('options = ', options)
+
         })();
         return () => {
             active = false;
@@ -68,81 +82,147 @@ export default function Asynchronous() {
             setOptions([]);
         }
     }, [open]);
+    // console.log(topFilms)
+
+    let arrayList = options.map((element, index) => {
+ return (
+     <>
+         <Link href={`/about?${element.imdbID}`} target="_blank" >
+
+         <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+            <ListItem alignItems="flex-start">
+                <ListItemAvatar>
+                    <Avatar alt="Remy Sharp" src={element.Poster} />
+                </ListItemAvatar>
+                <ListItemText
+                    primary={element.Title }
+                    secondary={
+                        <React.Fragment>
+                            <Typography
+                                sx={{ display: 'inline' }}
+                                component="span"
+                                variant="body2"
+                                color="text.primary"
+                            >
+                                Year
+                            </Typography>
+                            {element.Year}
+                        </React.Fragment>
+                    }
+                />
+            </ListItem>
+            <Divider variant="inset" component="li" />
+        </List>
+         </Link>
+     </>
+ )
+    })
 
     return (
         <div>
-        <Autocomplete
-            // defaultValue={{ "The Godfather"}}
-            // value={{inputAutoValue}}
-            inputValue={inputAutoValue}
 
-            style={{ backgroundColor: 'white', color: 'black', border: 'yellow' }}
-            id="asynchronous-demo"
-            sx={{ width: 300 }}
-            open={open}
-            onOpen={() => {
-                setOpen(true);
-            }}
-            onClose={() => {
-                setOpen(false);
-            }}
-            onInputChange={(event, newInputValue) => {
-                console.log('newInputValue = ', newInputValue)
-                setInputValue(newInputValue);
-                console.log('inputValue = ', inputValue)
-                setInputAutoValue(newInputValue)
-                console.log('inputAutoValue = ', inputAutoValue)
+            <TextField
+                style={{ backgroundColor: 'white', color: 'black', border: 'yellow'}}
 
-            }}
-            isOptionEqualToValue={(option, value) => option.Title === value.Title}
-            getOptionLabel={(option) => option.Title }
-            options={options}
-            loading={loading}
+                hiddenLabel
+                id="filled-hidden-label-small"
+                defaultValue={inputAutoValue}
+                variant="filled"
+                size="small"
+                onChange={(event) => {
+                    // console.log('newInputValue = ', newInputValue)
+                    setInputValue(event.target.value);
+                    setInputAutoValue(event.target.value)
+                    // console.log('inputValue = ', inputValue)
+                    // setInputAutoValue(event.target.value)
+                    console.log('event.target.value: ',event.target.value)
+                }}
+
+            />
+            {arrayList}
 
 
-            renderInput={(params) => (
-                <TextField
-                    {...params}
-                    label="Movie name"
-                    InputProps={{
-                        ...params.InputProps,
-                        endAdornment: (
-                            <React.Fragment>
-                                {loading ? <CircularProgress color="inherit" size={20} /> : null}
-                                {params.InputProps.endAdornment}
-                            </React.Fragment>
-                       ),
-                    }}
-                />
-            )}
-            renderOption={(props, option) => {
-                // const linkMovieAbout = `https://omdbapi.com/?apikey=ea5666d3&i=${option.imdbID}&plot=full`;
-                const linkAbout = `/about?${option.imdbID}`
-                return (
-                    <li {...props}>
-                        <Grid container alignItems="center">
-                            <Grid item sx={{ display: 'flex', width: 144  }}>
-                                <img
-                                    src={option.Poster !== 'N/A'? option.Poster : "image_not_found.png"}
-                                    alt={option.Title}
-                                    width="40px"
-                                    loading="lazy"
-                                />
-                            </Grid>
-                            <Grid item sx={{ width: 'calc(100% - 44px)', wordWrap: 'break-word' }}>
-                                {option.Year}
-                                <Typography variant="body2" color="text.secondary">
 
-                                    <Link href={linkAbout} target="_blank" > {option.Title}</Link>
+        {/*    <Autocomplete*/}
+        {/*    // defaultValue={{ "The Godfather"}}*/}
+        {/*    // value={{inputAutoValue}}*/}
+        {/*    inputValue={inputAutoValue}*/}
 
-                                </Typography>
-                            </Grid>
-                        </Grid>
-                    </li>
-                    // <About link={movieLink}/>
-                );
-              }}
-        />
+        {/*    style={{ backgroundColor: 'white', color: 'black', border: 'yellow' }}*/}
+        {/*    id="asynchronous-demo"*/}
+        {/*    sx={{ width: 300 }}*/}
+        {/*    open={open}*/}
+        {/*    onOpen={() => {*/}
+        {/*        setOpen(true);*/}
+        {/*    }}*/}
+        {/*    onClose={() => {*/}
+        {/*        setOpen(false);*/}
+        {/*    }}*/}
+        {/*    onInputChange={(event, newInputValue) => {*/}
+        {/*        console.log('newInputValue = ', newInputValue)*/}
+        {/*        setInputValue(newInputValue);*/}
+        {/*        console.log('inputValue = ', inputValue)*/}
+        {/*        setInputAutoValue(newInputValue)*/}
+        {/*        console.log('inputAutoValue = ', inputAutoValue)*/}
+
+        {/*    }}*/}
+        {/*    isOptionEqualToValue={(option, value) => option.Title === value.Title}*/}
+        {/*    getOptionLabel={(option) => option.Title }*/}
+        {/*    options={options}*/}
+        {/*    loading={loading}*/}
+
+
+        {/*    renderInput={(params) => (*/}
+        {/*        <TextField*/}
+        {/*            {...params}*/}
+        {/*            label="Movie name"*/}
+        {/*            InputProps={{*/}
+        {/*                ...params.InputProps,*/}
+        {/*                endAdornment: (*/}
+        {/*                    <React.Fragment>*/}
+        {/*                        {loading ? <CircularProgress color="inherit" size={20} /> : null}*/}
+        {/*                        {params.InputProps.endAdornment}*/}
+        {/*                    </React.Fragment>*/}
+        {/*               ),*/}
+        {/*            }}*/}
+        {/*        />*/}
+        {/*    )}*/}
+
+
+
+
+
+
+
+        {/*    renderOption={(props, option) => {*/}
+        {/*        // const linkMovieAbout = `https://omdbapi.com/?apikey=ea5666d3&i=${option.imdbID}&plot=full`;*/}
+        {/*        const linkAbout = `/about?${option.imdbID}`*/}
+        {/*        return (*/}
+        {/*            <li {...props}>*/}
+        {/*                <Grid container alignItems="center">*/}
+        {/*                    <Grid item sx={{ display: 'flex', width: 144  }}>*/}
+        {/*                        <img*/}
+        {/*                            src={option.Poster !== 'N/A'? option.Poster : "image_not_found.png"}*/}
+        {/*                            alt={option.Title}*/}
+        {/*                            width="40px"*/}
+        {/*                            loading="lazy"*/}
+        {/*                        />*/}
+        {/*                    </Grid>*/}
+        {/*                    <Grid item sx={{ width: 'calc(100% - 44px)', wordWrap: 'break-word' }}>*/}
+        {/*                        {option.Year}*/}
+        {/*                        <Typography variant="body2" color="text.secondary">*/}
+
+        {/*                            <Link href={linkAbout} target="_blank" > {option.Title}</Link>*/}
+
+        {/*                        </Typography>*/}
+        {/*                    </Grid>*/}
+        {/*                </Grid>*/}
+        {/*            </li>*/}
+        {/*            // <About link={movieLink}/>*/}
+        {/*        );*/}
+        {/*      }}*/}
+        {/*/>*/}
+
         </div>
     );
 }
